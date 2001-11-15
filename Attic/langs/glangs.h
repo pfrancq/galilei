d3e2ae3e6e77bcb691f6e15a009ddd02b390444c
jlamoral <jlamoral@da7721e6-cd2b-0410-b678-e5eefa682a33>
2001-11-15 08:41:37 +0000

    test to connection to database

--- Attic/langs/glangs.h
+++ Attic/langs/glangs.h
@@ -1,31 +1,31 @@
 /*
 
-	GALILEI Research Project
+        GALILEI Research Project
 
-	gwordref.h
+        gwordref.h
 
-	Basic Information - Implementation.
+        Basic Information - Implementation.
 
-	(C) 2001 by P. Francq.
+        (C) 2001 by P. Francq.
 
-	Version $Revision$
+        Version $Revision$
 
-	Last Modify: $Date$
+        Last Modify: $Date$
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Library General Public
-	License as published by the Free Software Foundation; either
-	version 2.0 of the License, or (at your option) any later version.
+        This library is free software; you can redistribute it and/or
+        modify it under the terms of the GNU Library General Public
+        License as published by the Free Software Foundation; either
+        version 2.0 of the License, or (at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Library General Public License for more details.
+        This library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        Library General Public License for more details.
 
-	You should have received a copy of the GNU Library General Public
-	License along with this library, as a file COPYING.LIB; if not, write
-	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-	Boston, MA  02111-1307  USA
+        You should have received a copy of the GNU Library General Public
+        License along with this library, as a file COPYING.LIB; if not, write
+        to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+        Boston, MA  02111-1307  USA
 
 */
 
@@ -67,9 +67,9 @@ class GLangs : public RContainer<GLang,unsigned,true,true>
 public:
   GLangs(unsigned nb) throw(bad_alloc);
   GLang* GetLang(const char* code);   // Return a pointer to the language
-  virtual void Load(void) throw(bad_alloc,GException)=0;  //  Load the Languages
+  //virtual void Load(void) throw(bad_alloc,GException)=0;  //  Load the Languages
  // void CreateDicsStops(GDicts *dicts,GDicts *stops) throw(bad_alloc,GException);
-	virtual ~GLangs(void) {}
+        virtual ~GLangs(void) {}
 };
 
 }
