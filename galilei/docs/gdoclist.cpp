9980ca87e68b1c0baa570a123b296719efe1d197
pfrancq <pfrancq@da7721e6-cd2b-0410-b678-e5eefa682a33>
2001-12-04 10:54:37 +0000

    - New class (not correct in this version).

---
+++ galilei/docs/gdoclist.cpp
@@ -0,0 +1,226 @@
+/*
+
+	GALILEI Research Project
+
+	gdoc.cpp
+
+	Document - Implementation.
+
+	(C) 2001 by P. Francq.
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+*/
+
+
+
+////-----------------------------------------------------------------------------
+//// include files for ANSI C/C++
+//#include <ctype.h>
+//
+//
+////-----------------------------------------------------------------------------
+//// include files for GALILEI
+//#include <gdocs/gdoc.h>
+//#include <gdocs/gdocxml.h>
+//#include <gsessions/gsession.h>
+//using namespace GALILEI;
+//using namespace RXML;
+//
+//
+//
+////-----------------------------------------------------------------------------
+////
+//// class GDoc
+////
+////-----------------------------------------------------------------------------
+//
+////-----------------------------------------------------------------------------
+//GALILEI::GDoc::GDoc (const RString& url,const unsigned int id,const unsigned int nbdiff) throw(bad_alloc)
+//  : URL(url),Id(id),Words(nbdiff),NbWords(0),NbDiffWords(),Lang(0),Calc(true), bSave(false)
+//{
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//GALILEI::GDoc::GDoc(const RString& url,const unsigned int id,GLang* lang,const unsigned int nb,const unsigned int nbdiff) throw(bad_alloc)
+//	: URL(url), Id(id), Words(nbdiff),NbWords(nb), NbDiffWords(nbdiff),Calc(true), bSave(false)
+//{
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//int GALILEI::GDoc::Compare(const GDoc& doc)
+//{
+//  return(Id-doc.Id);
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//int GALILEI::GDoc::Compare(const GDoc* doc)
+//{
+//  return(Id-doc->Id);
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//int GALILEI::GDoc::Compare(const unsigned id)
+//{
+//  return(Id-id);
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//bool GALILEI::GDoc::ExtractWord(const char* &ptr,RString& word)
+//{
+//	unsigned len=0;
+//	const char* begin;
+//	bool Cont=true;
+//
+//	// Skip spaces and punctation.
+//	while((*ptr)&&(!isalnum(*ptr)))
+//		ptr++;
+//	begin=ptr;
+//
+//	// Read the word
+//	while((*ptr)&&Cont)
+//	{
+//		// Read word --> look for a non alphanumeric character.
+//		while((*ptr)&&(isalnum(*ptr)))
+//		{
+//			len++;
+//			ptr++;
+//		}
+//		Cont=false;   // Normally, end of the word.
+//		if(!(*ptr)) continue;
+//
+//		// If the next character is in {-'./\@} and no blank space after,
+//		// then continue the word.
+//		if((strchr("\'-.@\\/",*ptr))&&(isalnum(*(ptr+1))))
+//		{
+//			Cont=true;
+//			ptr++;
+//			len++;
+//		}
+//	}
+//
+//	// If len is not null, copy result in word and return true, else return
+//	// false.
+//	if(!len) return(false);
+//	word.Copy(begin,len);
+//	word.StrLwr();
+//	return(true);
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//int GALILEI::GDoc::AnalyseTagForStopKwd(RXMLTag* tag,GDict* stop)
+//{
+//	unsigned nb=0;
+//	const char* ptr;
+//	RString word(50);
+//
+//	if(tag->GetName()=="Sentence")
+//	{
+//		ptr=tag->GetContent();
+//		while(*ptr)
+//		{
+//			if((ExtractWord(ptr,word))&&(stop->IsIn(word)))
+//				nb++;
+//		}
+//	}
+//	else
+//	{
+//		for(tag->Start();!tag->End();tag->Next())
+//			nb+=AnalyseTagForStopKwd((*tag)(),stop);
+//	}
+//	return(nb);
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//void GALILEI::GDoc::AnalyseContentTag(RXMLTag* tag,GDict* stop,GDict* dic) throw(GException)
+//{
+//	cout<<tag->GetName()<<endl;
+//	const char* ptr;
+//	RString word(50);
+//	GIWordOccur* Occur;
+//
+//	if(tag->GetName()=="Sentence")
+//	{
+//		ptr=tag->GetContent();
+//		while(*ptr)
+//		{
+//			if(ExtractWord(ptr,word))
+//			{
+//				NbWords++;
+//				if(!stop->IsIn(word))
+//				{
+//					Occur=Words.GetPtr(dic->GetId(word));
+//					if(!Occur->GetNbOccurs()) NbDiffWords++;
+//					Occur->IncOccurs();
+//				}
+//			}
+//		}
+//	}
+//	else
+//	{
+//		for(tag->Start();!tag->End();tag->Next())
+//			AnalyseContentTag((*tag)(),stop,dic);
+//	}
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//void GALILEI::GDoc::Analyse(GDocXML* xml,GSession* session) throw(GException)
+//{
+//	RXMLTag* content;
+//	int max,act;
+//	GDict* stop;
+//	GDict* dic;
+//
+//	cout<<"Analyse: "<<URL()<<endl;
+//	content=xml->GetContent();
+//	RAssert(content);
+//
+//	// Find Language with the maximal number of words of the stoplist contained
+//	// in the document.
+//	cout<<"Nb Words: "<<AnalyseTagForStopKwd(content,0)<<endl;
+//	max=0;
+//	for(session->Langs->Start();!session->Langs->End();session->Langs->Next())
+//	{
+//		act=AnalyseTagForStopKwd(content,dic=session->GetStop((*session->Langs)()));
+//		if(act>max)
+//		{
+//			Lang=(*session->Langs)();
+//			max=act;
+//			stop=dic;
+//		}
+//	}
+//	if(!Lang) return;
+//
+//	// Analyse it
+//	Words.Clear();
+//	NbWords=NbDiffWords=0;
+//	AnalyseContentTag(content,stop,session->GetDic(Lang));
+//	Calc=false;
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//void GALILEI::GDoc::AddWord(const unsigned int id,const unsigned int nb)
+//{
+//	Words.InsertPtr(new GIWordOccur(id,nb));
+//}
+//
+//
+////-----------------------------------------------------------------------------
+//GALILEI::GDoc::~GDoc(void)
+//{
+//}
+//
+//
+//
+//
---
+++ galilei/docs/gdoclist.h
@@ -0,0 +1,219 @@
+/*
+
+	GALILEI Research Project
+
+	gdoc.h
+
+	Document - Header.
+
+	(C) 2001 by P. Francq.
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef GDocH
+#define GDocH
+
+
+//-----------------------------------------------------------------------------
+// include files for R Project
+#include <rstd/rstring.h>
+#include <rxml/rxmltag.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for GALILEI
+#include <galilei.h>
+#include <glangs/gdict.h>
+#include <ginfos/giwordoccurs.h>
+#include <glangs/glang.h>
+
+
+//-----------------------------------------------------------------------------
+namespace GALILEI{
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// Forward declaration
+class GDocXML;
+class GSession;
+class GDocs;
+
+
+//-----------------------------------------------------------------------------
+/**
+* The GDoc class provides a representation of the analysis of a document.
+* @author Pascal Francq
+* @short Document.
+*/
+class GDoc
+{
+	/**
+	* URL of the document.
+	*/
+	RStd::RString URL;
+
+	/**
+	* Identifier of the document.
+	*/
+	unsigned Id;
+
+	/**
+	* Information about the words containted in the document. Actually, only
+	* the number of occurences of the words are used.
+	*/
+	GIWordOccurs Words;
+
+	/**
+	* Total number of words.
+	*/
+	unsigned NbWords;
+
+	/**
+	* Number of different words.
+	*/
+	unsigned NbDiffWords;
+
+	/**
+	* Pointer to the language.
+	*/
+	GLang* Lang;
+
+	/**
+	* Must the doc be analysed?
+	*/
+	bool Calc;
+
+	/**
+	* Must the doc be saved?
+	*/
+	bool bSave;
+
+public:
+
+	/**
+	* Construct the document.
+	* @param url            URL of the document.
+	* @param id             Identifier of the document.
+	* @param nbdiff         Number of different words appearing in the
+	*                       document.
+	*/
+	GDoc(const RStd::RString& url,const unsigned int id,const unsigned int nbdiff=500) throw(bad_alloc);
+
+	/**
+	* Construct the document.
+	* @param url            URL of the document.
+	* @param id             Identifier of the document.
+	* @param lang           Language of the document.
+	* @param nb             Total number of words in the document.
+	* @param nbdiff         Number of different words appearing in the
+	*                       document.
+	*/
+	GDoc(const RStd::RString& url,const unsigned int id,GLang* lang,const unsigned int nb,const unsigned int nbdiff) throw(bad_alloc);
+
+	/**
+	* Compare function needed by RStd::RContainer.
+	* @param doc            Document used for the comparaison.
+	*/
+	int Compare(const GDoc& doc);
+
+	/**
+	* Compare function needed by RStd::RContainer.
+	* @param doc            Pointer to the document used for the comparaison.
+	*/
+	int Compare(const GDoc* doc);
+
+	/**
+	* Compare function needed by RStd::RContainer.
+	* @param id            Identificator used for the comparaison.
+	*/
+	int Compare(const unsigned id);
+
+protected:
+
+	/**
+	* This function construct a word.
+	* @param ptr            Pointer that will be moved to the next word.
+	* @param word           Where to store the word.
+	* @returns true if a word was extract.
+	*/
+	bool ExtractWord(const char* &ptr,RStd::RString& word);
+
+public:
+
+	/**
+	* Analyse a tag to find how much words are in a stoplist.
+	* @param tag            Tag to analyse.
+	* @param stop           Stoplist to use.
+	*/
+	int AnalyseTagForStopKwd(RXML::RXMLTag* tag,GDict* stop);
+
+	/**
+	* Analyse a content tag.
+	* @param tag            Tag to analyse.
+	* @param stop           Stoplist to use.
+	* @param dic            Dictionnary to use.
+	*/
+	void AnalyseContentTag(RXML::RXMLTag* tag,GDict* stop,GDict* dic) throw(GException);
+
+	/**
+	* Analyse a XML representation of a document for a session and store the
+	* results in this document.
+	* @param xml            XML Representation used.
+	* @param session        Corresponding session.
+	*/
+	void Analyse(GDocXML* xml,GSession* session) throw(GException);
+
+	/**
+	* Add a word with a certain occurences in the document.
+	* @param id             Identificator of the word.
+	* @param nb             Occurences of the word.
+	*/
+	void AddWord(const unsigned int id,const unsigned int nb);
+
+	/**
+	* @return Total number of words in the documents.
+	*/
+	unsigned int GetNbWords(void) {return(NbWords);}
+
+	/**
+	* @return Number of different words in the documents.
+	*/
+	unsigned int GetNbDiffWords(void) {return(NbDiffWords);}
+
+	/**
+	* @return True if the document has to be calc.
+	*/
+	bool MustAnalyse(void) {return(Calc);}
+
+	/**
+	* @return Pointer to the language of the document.
+	*/
+	GLang* GetLang(void) {return(Lang);}
+
+	/**
+	* @return Identificator of the document.
+	*/
+	unsigned int GetId(void) {return(Id);}
+
+	/**
+	* Destruct the document.
+	*/
+	virtual ~GDoc(void);
+
+	friend class GWordCalcs;
+};
+
+
+}  //-------- End of namespace GALILEI ----------------------------------------
+
+
+//-----------------------------------------------------------------------------
+#endif
