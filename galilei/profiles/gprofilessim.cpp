4024122dfe4777009fefe2339902a10a1ec918fa
pfrancq <pfrancq@da7721e6-cd2b-0410-b678-e5eefa682a33>
2002-03-05 18:45:11 +0000

    - Hold the profiles similarities.

---
+++ galilei/profiles/gprofilessim.cpp
@@ -0,0 +1,141 @@
+/*
+
+	GALILEI Research Project
+
+	GProfilesSim.cpp
+
+	Similarities between profiles - Implementation
+
+	(C) 2002 by P. Francq.
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+// include files for GALILEI
+#include <profiles/gprofilessim.h>
+#include <profiles/gsubprofile.h>
+using namespace GALILEI;
+
+
+
+//-----------------------------------------------------------------------------
+//
+// class GSim
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+class GSim
+{
+public:
+	unsigned int Id;
+	double Sim;
+
+	GSim(unsigned int id,double s) : Id(id), Sim(s) {};
+	int Compare(const GSim* s) const {return(Id-s->Id);}
+	int Compare(const GSim& s) const {return(Id-s.Id);}
+	int Compare(const unsigned int id) const {return(Id-id);}
+};
+
+
+
+//-----------------------------------------------------------------------------
+//
+// class GSims
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+class GALILEI::GProfilesSim::GSims : public RStd::RContainer<GSim,unsigned int,true,true>
+{
+public:
+	unsigned int Id;
+
+	GSims(unsigned int id,unsigned int max) throw(bad_alloc);
+	int Compare(const GSims* s) const {return(Id-s->Id);}
+	int Compare(const GSims& s) const {return(Id-s.Id);}
+	int Compare(const unsigned int id) const {return(Id-id);}
+};
+
+//-----------------------------------------------------------------------------
+GALILEI::GProfilesSim::GSims::GSims(unsigned int id,unsigned int max) throw(bad_alloc)
+	: RStd::RContainer<GSim,unsigned int,true,true>(max,max/2), Id(id)
+{
+}
+
+
+
+//-----------------------------------------------------------------------------
+//
+// class GProfilesSim
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+GALILEI::GProfilesSim::GProfilesSim(RStd::RContainer<GSubProfile,unsigned int,false,true>* s) throw(bad_alloc)
+	: Sims(s->NbPtr,s->NbPtr/2)
+{
+	GSubProfileCursor Cur1;
+	GSubProfileCursor Cur2;
+	unsigned int i,j;
+	GSims* sim;
+
+	Cur1.Set(s);
+	Cur2.Set(s);
+	for(Cur1.Start(),i=0,j=Cur1.GetNb();--j;Cur1.Next(),i++)
+	{
+		Sims.InsertPtr(sim=new GSims(Cur1()->GetId(),j));
+		for(Cur2.GoTo(i+1);!Cur2.End();Cur2.Next())
+		{
+			sim->InsertPtr(new GSim(Cur2()->GetId(),Cur1()->Similarity(Cur2())));
+		}
+	}
+}
+
+
+//-----------------------------------------------------------------------------
+double GALILEI::GProfilesSim::GetSim(unsigned int i,unsigned int j)
+{
+	GSims* s;
+	GSim* s2;
+	unsigned int tmp;
+
+	if(i>j)
+	{
+		tmp=i;
+		i=j;
+		j=tmp;
+	}
+	s=Sims.GetPtr<unsigned int>(i);
+	if(!s) return(0.0);
+	s2=s->GetPtr<unsigned int>(j);
+	if(!s2) return(0.0);
+	return(s2->Sim);
+}
+
+
+//-----------------------------------------------------------------------------
+GALILEI::GProfilesSim::~GProfilesSim(void)
+{
+}
---
+++ galilei/profiles/gprofilessim.h
@@ -0,0 +1,92 @@
+/*
+
+	GALILEI Research Project
+
+	GProfilesSim.h
+
+	Similarities between profiles - Header
+
+	(C) 2002 by P. Francq.
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef GProfilesSimH
+#define GProfilesSimH
+
+
+//-----------------------------------------------------------------------------
+// include files for GALILEI
+#include <galilei.h>
+
+
+//-----------------------------------------------------------------------------
+namespace GALILEI{
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+/**
+* The GProfilesSim class provides a representation for all the similarities
+* between the profiles, i.e. the subprofiles of a given language.
+* @author Pascal Francq
+* @short Profiles Similarities.
+*/
+class GProfilesSim
+{
+	class GSims;
+
+	/**
+	* The similarities.
+	*/
+	RStd::RContainer<GSims,unsigned int,true,true> Sims;
+
+public:
+
+	/**
+	* Constructor.
+	* @param s              Subprofiles of the system.
+	*/
+	GProfilesSim(RStd::RContainer<GSubProfile,unsigned int,false,true>* s) throw(bad_alloc);
+
+	/**
+	* Get the similarities between two profiles, i.e. the subprofiles of a same
+	* language.
+	* @param i              Identificator of the first subprofile.
+	* @param j              Identificator of the second subprofile.
+	* @return double.
+	*/
+	double GetSim(unsigned int i,unsigned int j);
+
+	/**
+	* Destructor.
+	*/
+	~GProfilesSim(void);
+};
+
+
+}  //-------- End of namespace GALILEI ----------------------------------------
+
+
+//-----------------------------------------------------------------------------
+#endif
