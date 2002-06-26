394117004b151079b9620f7c14fcdefdba92c209
pfrancq <pfrancq@3d853c18-d22d-0410-b50c-bba9b3355d98>
2002-06-26 09:57:25 +0000

    - New headers information in C++ files. - New similarity criterion measure.

--- html/gfilterhtml.cpp
+++ html/gfilterhtml.cpp
@@ -6,7 +6,10 @@
 
 	A HTML filter - Implementation.
 
-	(C) 2001 by Pascal Francq
+	Copyright 2001 by the Université Libre de Bruxelles.
+
+	Authors:
+		Pascal Francq (pfrancq@ulb.ac.be).
 
 	Version $Revision$
 
