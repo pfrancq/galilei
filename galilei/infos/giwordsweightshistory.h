9041d53ef5c9d28968c8a5aa9a92f198aad2785c
dwartel <dwartel@da7721e6-cd2b-0410-b678-e5eefa682a33>
2003-03-11 17:21:15 +0000

    - new files for history added

---
+++ galilei/infos/giwordsweightshistory.h
@@ -0,0 +1,150 @@
+/*
+
+	GALILEI Research Project
+
+	GIWordsWeights.h
+
+	Weights of a list of words - Header.
+
+	Copyright 2002 by the Université Libre de Bruxelles.
+
+	Authors:
+		Pascal Francq (pfrancq@ulb.ac.be).
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef GIWordsWeightsHistoryH
+#define GIWordsWeightsHistoryH
+
+
+//-----------------------------------------------------------------------------
+// include files for R Project
+#include <rstd/rcontainer.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for GALILEI
+#include <galilei.h>
+#include <profiles/gsubprofile.h>
+#include <infos/ginfo.h>
+#include <infos/giwordsweights.h>
+
+
+//-----------------------------------------------------------------------------
+namespace GALILEI{
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+/**
+* The GGroupsHistory class provides a representation for all the historic subprofile 
+* @author David Wartel
+* @short historic subprofile.
+*/
+class GIWordsWeightsHistory : public GIWordsWeights
+{
+
+private :
+
+	/**
+	* SubProfile.
+	*/
+	 GSubProfile* SubProfile;
+
+	 /**
+	 * parent
+	 */
+	 GGroupHistory* Parent;
+
+	 /**
+	 * is the subprofile well grouped?
+	 */
+	 bool WellGrouped;
+
+public :
+
+	/**
+	* constructor
+	*/
+	GIWordsWeightsHistory(GSubProfile* sub, unsigned int nb) : GIWordsWeights(nb) {SubProfile=sub; WellGrouped=false;}
+
+	/**
+	* get the subprofile.
+	*/
+	GSubProfile* GetSubProfile(void) {return SubProfile;}
+
+	/**
+	*  get the id.
+	*/
+	unsigned int GetId(void) {return SubProfile->GetId();}
+
+	/**
+	*  set the parent.
+	*/
+	void SetParent(GGroupHistory* p) {Parent=p;}
+
+	/**
+	*  return true if the subprofile is well-grouped.
+	*/
+	bool IsWellGrouped(void) {if (WellGrouped) return true; else return false;;}
+
+	/**
+	*  set tthe stauts (well grouped- not well grouped) of the subprofile..
+	*/
+	void SetWellGrouped(bool b) {WellGrouped=b;}
+
+	/**
+	*  get the parent.
+	*/
+	GGroupHistory* GetParent(void) {return Parent;}
+
+	/**
+	* Compare method needed by RStd::RContainer.
+	*/
+	int Compare(const GIWordsWeightsHistory& giwwh) const {return(SubProfile->GetId()-giwwh.SubProfile->GetId());}
+
+	/**
+	* Compare method needed by RStd::RContainer.
+	*/
+	 int Compare(const GIWordsWeightsHistory* giwwh) const {return(SubProfile->GetId()-giwwh->SubProfile->GetId());}
+
+	/**
+	* Compare method needed by RStd::RContainer.
+	*/
+	int Compare(unsigned int id) const {return(SubProfile->GetId()-id);}
+
+	/**
+	*   Destructor
+	*/
+	~GIWordsWeightsHistory(void) {};
+
+};
+
+
+}  //-------- End of namespace GALILEI ----------------------------------------
+
+
+//-----------------------------------------------------------------------------
+#endif
