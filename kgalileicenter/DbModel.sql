cd8786c11c9e93ce78a963824b30422cd6a79782
pfrancq <pfrancq@038c31d4-cd2b-0410-b4d5-8dc527888777>
2003-06-25 15:20:09 +0000

    Add scripts for creating MySQL databases.

---
+++ kgalileicenter/DbModel.sql
@@ -0,0 +1,332 @@
+
+-- author Vandaele Valery     vavdaele@ulb.ac.be
+--------------------------------------------------------------------------
+
+-- this file contains all the command to be use to create a database 
+-- with all required tables and fields.
+--------------------------------------------------------------------------
+
+-- This file can be modified but take care : the database created with this  
+-- file must respect GALILEI exigences !!!
+
+---------------------------------------------------------------------------- 
+---------------------------------------------------------------------------- 
+-- MODIFICATION OF THIS FILE CAN DRIVE TO THE INCAPACITY OF CREATE NEW EMPTY
+-- DATABASES IN GALILEI ENVIRONEMENT !!!
+---------------------------------------------------------------------------- 
+---------------------------------------------------------------------------- 
+  
+--
+-- Table structure for table 'engroupsbykwds'
+--
+
+CREATE TABLE engroupsbykwds (
+  groupid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  occurs double default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'enhtmlsbykwds'
+--
+
+CREATE TABLE enhtmlsbykwds (
+  htmlid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  occurs double(20,10) default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'enkwds'
+--
+
+CREATE TABLE enkwds (
+  kwdid int(11) NOT NULL auto_increment,
+  kwd varchar(50) default NULL,
+  PRIMARY KEY  (kwdid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'enstopkwds'
+--
+
+CREATE TABLE enstopkwds (
+  kwdid int(11) NOT NULL auto_increment,
+  kwd varchar(50) NOT NULL default '',
+  PRIMARY KEY  (kwdid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'ensubprofilesbykwds'
+--
+
+CREATE TABLE ensubprofilesbykwds (
+  subprofileid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  weight float NOT NULL default '0'
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frgroupsbykwds'
+--
+
+CREATE TABLE frgroupsbykwds (
+  groupid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  occurs double default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frhtmlsbykwds'
+--
+
+CREATE TABLE frhtmlsbykwds (
+  htmlid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  occurs int(20) default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frkwds'
+--
+
+CREATE TABLE frkwds (
+  kwdid int(11) NOT NULL auto_increment,
+  kwd varchar(50) default NULL,
+  PRIMARY KEY  (kwdid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frstopkwds'
+--
+
+CREATE TABLE frstopkwds (
+  kwdid int(11) NOT NULL auto_increment,
+  kwd varchar(50) NOT NULL default '',
+  PRIMARY KEY  (kwdid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frsubprofilesbykwds'
+--
+
+CREATE TABLE frsubprofilesbykwds (
+  subprofileid int(11) NOT NULL default '0',
+  kwdid int(11) NOT NULL default '0',
+  weight float NOT NULL default '0'
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'groups'
+--
+
+CREATE TABLE groups (
+  groupid int(11) NOT NULL auto_increment,
+  langid char(2) NOT NULL default '',
+  PRIMARY KEY  (groupid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'htmls'
+--
+
+CREATE TABLE htmls (
+  htmlid int(11) NOT NULL auto_increment,
+  html varchar(250) NOT NULL default '',
+  title varchar(100) default NULL,
+  langid char(2) default NULL,
+  updated date default NULL,
+  calculated date default NULL,
+  mimetype varchar(50) default NULL,
+  failed int(11) default '0',
+  n int(20) default '0',
+  ndiff int(20) default '0',
+  v int(20) default '0',
+  vdiff int(20) default '0',
+  PRIMARY KEY  (htmlid),
+  KEY index_html (html)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'htmlsbyprofiles'
+--
+
+CREATE TABLE htmlsbyprofiles (
+  htmlid int(11) NOT NULL default '0',
+  profileid int(11) NOT NULL default '0',
+  judgement char(2) NOT NULL default '',
+  when2 date default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'idealgroup'
+--
+
+CREATE TABLE idealgroup (
+  profileid int(11) NOT NULL auto_increment,
+  langid char(2) NOT NULL default '',
+  groupid int(11) NOT NULL default '0',
+  PRIMARY KEY  (profileid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'languages'
+--
+
+CREATE TABLE languages (
+  langid char(2) default NULL,
+  language char(30) default NULL
+) TYPE=MyISAM;
+
+
+INSERT INTO languages VALUES ('en','English');
+INSERT INTO languages VALUES ('fr','Français');
+
+--
+-- Table structure for table 'profiles'
+--
+
+CREATE TABLE profiles (
+  profileid int(11) NOT NULL auto_increment,
+  userid int(11) NOT NULL default '0',
+  description varchar(100) default NULL,
+  updated date default NULL,
+  calculated date default NULL,
+  social tinyint(1) default '0',
+  topicid int(11) NOT NULL default '0',
+  PRIMARY KEY  (profileid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'subprofiles'
+--
+
+CREATE TABLE subprofiles (
+  subprofileid int(11) NOT NULL auto_increment,
+  profileid int(11) NOT NULL default '0',
+  langid char(2) NOT NULL default '',
+  attached date default NULL,
+  groupid int(11) default '0',
+  calculated date default NULL,
+  state int(2) default '0',
+  PRIMARY KEY  (subprofileid)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'tempchromo'
+--
+
+CREATE TABLE tempchromo (
+  chromoid int(11) default NULL,
+  groupid int(11) default NULL,
+  lang char(2) default NULL,
+  subprofileid int(11) default NULL,
+  tempid tinyint(1) default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'topics'
+--
+
+CREATE TABLE topics (
+  topicid int(11) NOT NULL auto_increment,
+  name varchar(250) NOT NULL default '',
+  parent int(11) default '0',
+  langid char(2) NOT NULL default '',
+  used char(1) default '0',
+  PRIMARY KEY  (topicid)
+) TYPE=MyISAM;
+
+--
+-- Table structure for table 'topicsbyhtmls'
+--
+
+CREATE TABLE topicsbyhtmls (
+  topicid int(11) NOT NULL default '0',
+  htmlid int(11) NOT NULL default '0'
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'htmlsbylinks'
+--
+
+CREATE TABLE htmlsbylinks (
+  htmlid int(11) NOT NULL default '0',
+  linkid int(11) NOT NULL default '0',
+  occurs int(11) default NULL
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'historicgroups'
+--
+
+CREATE TABLE historicgroups (
+  historicid int(11),
+  groupid int(11),
+  lang char(2),
+  subprofileid int(11)
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'frhistoricsubprofiles'
+--
+
+CREATE TABLE frhistoricsubprofiles (
+  historicid int(11),
+  subprofileid int(11),
+  kwdid int(11),
+  weight float
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'enhistoricsubprofiles'
+--
+
+CREATE TABLE enhistoricsubprofiles (
+  historicid int(11),
+  subprofileid int(11),
+  kwdid int(11),
+  weight float
+) TYPE=MyISAM;
+
+
+--
+-- Table structure for table 'users'
+--
+
+CREATE TABLE users (
+  userid int(11) NOT NULL auto_increment,
+  user varchar(20) default NULL,
+  pwd varchar(20) default NULL,
+  fullname varchar(50) default NULL,
+  email varchar(50) default NULL,
+  title varchar(50) default NULL,
+  organisation varchar(100) default NULL,
+  address1 varchar(100) default NULL,
+  address2 varchar(100) default NULL,
+  city varchar(100) default NULL,
+  country varchar(50) default NULL,
+  PRIMARY KEY  (userid)
+) TYPE=MyISAM;
+
---
+++ kgalileicenter/DbUsers.sql
@@ -0,0 +1,50 @@
+
+-- author Vandaele Valery     vavdaele@ulb.ac.be
+--------------------------------------------------------------------------
+
+-- this file contains all the command to be use to fill the table "users"
+--------------------------------------------------------------------------
+
+-- This file can be modified but take care : the database created with this  
+-- file must respect GALILEI exigences !!!
+
+---------------------------------------------------------------------------- 
+---------------------------------------------------------------------------- 
+-- NEW USERS CAN BE ADD BUT TAKE CARE: EACH NEW ENTRY IN THIS TABLE MUST 
+-- SATISFY THE FIXED FIELDS:
+-- TABLES USERS:
+--	userid , must be different from all existing ones
+--	user ,   user nick name
+--	pwd ,	
+--	fullname,
+--	email ,
+--	title,
+--	organisation,
+--	address1 ,
+--	address2 ,
+--	city,
+--	country,
+
+---------------------------------------------------------------------------- 
+---------------------------------------------------------------------------- 
+  
+
+
+
+
+INSERT INTO users VALUES (1,'jlamoral','jlamoral','Julien Lamoral','jlamoral@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (2,'dwartel','dwartel','David Wartel','Dwartel@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (3,'mpaternostre','mpaternostre','Marjorie Paternostre','marjorie.paternostre@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (4,'pfrancq','pfrancq','Pascal Francq','pfrancq@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (5,'vvandaele','vvandaele','Valery Vandaele','vavdaele@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (6,'nkumps','nkumps','Nicolas Kumps','nkumps@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (7,'adelch','adelch','Alain Delchambre','adelch@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (8,'pdelit','pdelit','Pierre De Lit','pdelit@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (9,'lauquier','lauquier','Laurent Auquière','lauquier@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (10,'ben','ben','Benoit Lagrange','blagrange@ulb.ac.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (11,'napoleon','napoleon','Pierre Lambert','napoleon@waterloo.be','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+INSERT INTO users VALUES (12,'george','george','Marco Saerens','bush@whitehouse.com','Researcher','Université Libre de Bruxelles','Avenue F.D. Roosevelt, 50','CP 165/14','B-1050 Brussels','Belgium');
+
+
+
+
