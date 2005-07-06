0b3171babea08b88ca1882c10f974a46c0920d40
pfrancq <pfrancq@da7721e6-cd2b-0410-b678-e5eefa682a33>
2005-07-06 17:04:17 +0000

    - Bug fix when clearing a group.

--- galilei/groups/ggroups.cpp
+++ galilei/groups/ggroups.cpp
@@ -228,14 +228,11 @@ void GGroups::Clear(GLang* lang)
 
 	// Go through the groups and delete all invalid groups.
 	if(!grps) return;
-	cout<<"   "<<lang->GetCode()<<": "<<grps->GetNb()<<endl;
 	for(i=grps->GetNb()+1;--i;)
 	{
-		cout<<"       "<<i<<endl;
 		grp=(*grps)[0];
- 		grp->DeleteSubProfiles();
-		DeletePtr(grp);
 		grps->DeletePtr(grp);
+		DeletePtr(grp);
 	}
 }
 
@@ -243,7 +240,6 @@ void GGroups::Clear(GLang* lang)
 //------------------------------------------------------------------------------
 void GGroups::ClearGroups(void)
 {
-	cout<<"ClearGroups"<<endl;
 	RCursor<GGroupsLang> Groups(GroupsLang);
 	for(Groups.Start();!Groups.End();Groups.Next())
 	{
