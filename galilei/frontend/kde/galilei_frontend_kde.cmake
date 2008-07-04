1411072c7b16db47eec27b261340c92a2141349f
pfrancq <pfrancq@da7721e6-cd2b-0410-b678-e5eefa682a33>
2008-07-04 14:19:36 +0000

    - Update CMake file.

--- galilei/frontend/kde/galilei_frontend_kde.cmake
+++ galilei/frontend/kde/galilei_frontend_kde.cmake
@@ -1,5 +1,5 @@
 SET(galilei_frontend_kde_MOC_INCLUDES   qgprofile.h
-					qgdocxml.h)
+					qgdocxml.h qgcommunitieshistory.h)
 
 SET(galilei_frontend_kde_UI_INCLUDES dlgmeasure2elements.ui)
 
@@ -7,12 +7,14 @@ SET(galilei_frontend_kde_INST_INCLUDES ${galilei_frontend_kde_MOC_INCLUDES}
                                         qgprofile.h
                                         qgdocxml.h
                                         qgmeasure2elementsdlg.h
-                                        qlistviewitemtype.h)
+                                        qlistviewitemtype.h
+                                        qgcommunitieshistory.h)
 
 SET(galilei_frontend_kde_TARGET_SOURCES qgprofile.cpp
                                         qgdocxml.cpp
                                         qlistviewitemtype.cpp
-                                        qgmeasure2elementsdlg.cpp)
+                                        qgmeasure2elementsdlg.cpp
+                                        qgcommunitieshistory.cpp)
 
 ###This is not very simple way to produce moc file
 ## A macro must be added
@@ -31,16 +33,16 @@ INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR})
 INSTALL(FILES frontend/kde/nokonqueror.png
 		frontend/kde/xml_cdata_b.png
 		frontend/kde/xml_comment_b.png
-		frontend/kde/xml_contents_b.png  
-		frontend/kde/xml_element_b.png  
-		frontend/kde/xml_procinstr_b.png  
+		frontend/kde/xml_contents_b.png
+		frontend/kde/xml_element_b.png
+		frontend/kde/xml_procinstr_b.png
 		frontend/kde/xml_text_b.png
-		frontend/kde/xml_attribute.png  
-		frontend/kde/xml_cdata.png    
-		frontend/kde/xml_comment.png    
-		frontend/kde/xml_contents.png 
-		frontend/kde/xml_element.png 
-		frontend/kde/xml_procinstr.png 
+		frontend/kde/xml_attribute.png
+		frontend/kde/xml_cdata.png
+		frontend/kde/xml_comment.png
+		frontend/kde/xml_contents.png
+		frontend/kde/xml_element.png
+		frontend/kde/xml_procinstr.png
 		frontend/kde/xml_text.png
 
 		DESTINATION share/icons/hicolor/16x16/actions/)
