37bd6d6df7a160cfb6a768bde4ecd0cd098f02de
pfrancq <pfrancq@217aa3a9-d22d-0410-8724-f61aaa32e77c>
2008-07-07 08:28:58 +0000

    - Move from Automake/Autoconf to CMake.

---
+++ gravitation/frontend/gravitation_frontend.cmake
@@ -0,0 +1,5 @@
+IF(KDE3_FOUND)
+    SET(gravitation_AVAILABLE_FRONTENDS kde)
+    SET(gravitation_kde_DEPENDENCY rcorekde ${QT_AND_KDECORE_LIBS})
+    ADD_FRONTEND_SOURCE_FILE(gravitation_frontend gravitation kde gravitation_INCLUDES)
+ENDIF(KDE3_FOUND)
---
+++ gravitation/gravitation.cpp
@@ -0,0 +1,176 @@
+/*
+
+	GALILEI Research Project
+
+	GCommunityCalcGravitation.cpp
+
+	Group Description is Gravitational Point Computing Method - Implementation.
+
+	Copyright 2002-2008 by the Université Libre de Bruxelles.
+
+	Authors:
+		Pascal Francq (pfrancq@ulb.ac.be).
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+// include files for ANSI C/C++
+#include <stdlib.h>
+
+
+//-----------------------------------------------------------------------------
+//include files for GALILEI
+#include <gravitation.h>
+#include <gcommunity.h>
+#include <gsession.h>
+#include <gprofile.h>
+#include <gweightinfo.h>
+#include <gweightinfos.h>
+using namespace GALILEI;
+using namespace R;
+using namespace std;
+
+
+
+//-----------------------------------------------------------------------------
+//
+//  class GCommunityCalcGravitation
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+GCommunityCalcGravitation::GCommunityCalcGravitation(GFactoryCommunityCalc* fac)
+	: GCommunityCalc(fac), Infos(5000,2500), MaxNonZero(100), Order(0), Vector(5000), MaxOrderSize(5000)
+{
+	Order=new GWeightInfo*[MaxOrderSize];
+}
+
+
+//-----------------------------------------------------------------------------
+void GCommunityCalcGravitation::ApplyConfig(void)
+{
+	MaxNonZero=Factory->GetUInt("Max Size");
+}
+
+
+//-----------------------------------------------------------------------------
+void GCommunityCalcGravitation::Connect(GSession* session)
+{
+	GCommunityCalc::Connect(session);
+}
+
+
+//-----------------------------------------------------------------------------
+void GCommunityCalcGravitation::Disconnect(GSession* session)
+{
+	GCommunityCalc::Disconnect(session);
+}
+
+
+//-----------------------------------------------------------------------------
+void GCommunityCalcGravitation::Compute(GCommunity* grp)
+{
+	unsigned int i;
+	GWeightInfo* ins;
+	GWeightInfo** w;
+
+	// Clear the Vector.
+	Vector.Clear();
+	// Clear Infos
+	// Rem: Since Infos is not responsible for allocation/desallocation
+	//      -> parse it to prevent memory leaks
+	RCursor<GWeightInfo> Cur(Infos);
+	for(Cur.Start();!Cur.End();Cur.Next())
+		delete Cur();
+	Infos.Clear();
+
+	// If no subprofiles -> No relevant one.
+	if(!grp->GetNbObjs()) return;
+
+	// Go through the subprofiles and sum the weigths.
+	RCursor<GProfile> Prof(grp->GetObjs());
+	for(Prof.Start();!Prof.End();Prof.Next())
+	{
+		// Go trough the words of the current subprofile
+		RCursor<GWeightInfo> Cur(Prof()->GetInfos());
+		for(Cur.Start();!Cur.End();Cur.Next())
+		{
+			ins=Vector.GetInsertPtr(*Cur());
+			(*ins)+=Cur()->GetWeight();
+		}
+	}
+
+	// Copy the information of the relevant subprofile to the group.
+	if(Vector.GetNb()+1>MaxOrderSize)
+	{
+		if(Order) delete[] Order;
+		MaxOrderSize=static_cast<unsigned int>((Vector.GetNb()+1)*1.1);
+		Order=new GWeightInfo*[MaxOrderSize];
+	}
+	Vector.GetTab(Order);
+	if(Vector.GetNb())
+		qsort(static_cast<void*>(Order),Vector.GetNb(),sizeof(GWeightInfo*),GWeightInfos::sortOrder);
+	Order[Vector.GetNb()]=0;
+	if(MaxNonZero)
+	{
+		for(i=MaxNonZero+1,w=Order;(--i)&&(*w);w++)
+		{
+			if((*w)->GetWeight()>0)
+				Infos.InsertPtr(new GWeightInfo((*w)->GetConcept(),(*w)->GetWeight()/grp->GetNbObjs()));
+		}
+	}
+	else
+	{
+		for(w=Order;(*w);w++)
+		{
+			if((*w)->GetWeight()>0)
+				Infos.InsertPtr(new GWeightInfo((*w)->GetConcept(),(*w)->GetWeight()/grp->GetNbObjs()));
+		}
+	}
+
+	// Update the group.
+	grp->Update(&Infos,true);
+}
+
+
+//------------------------------------------------------------------------------
+void GCommunityCalcGravitation::CreateParams(RConfig* params)
+{
+	params->InsertParam(new RParamValue("Max Size",60));
+}
+
+
+//-----------------------------------------------------------------------------
+GCommunityCalcGravitation::~GCommunityCalcGravitation(void)
+{
+	// Clear Infos
+	// Rem: Since Infos is not responsible for allocation/desallocation
+	//      -> parse it to prevent memory leaks
+	RCursor<GWeightInfo> Cur(Infos);
+	for(Cur.Start();!Cur.End();Cur.Next())
+		delete Cur();
+	Infos.Clear();
+
+	if(Order) delete[] Order;
+}
+
+
+//------------------------------------------------------------------------------
+CREATE_COMMUNITYCALC_FACTORY("Gravitation Method",GCommunityCalcGravitation)
