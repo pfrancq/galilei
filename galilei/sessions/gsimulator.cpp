dffe07019dbcae0969c86eb7f4c14c1dc137b198
pfrancq <pfrancq@da7721e6-cd2b-0410-b678-e5eefa682a33>
2011-01-24 16:10:28 +0000

    - Update from 2010 tp 2011.

--- galilei/sessions/gsimulator.cpp
+++ galilei/sessions/gsimulator.cpp
@@ -6,7 +6,7 @@
 
 	Simulator - Implementation.
 
-	Copyright 2002-2010 by Pascal Francq (pascal@francq.info).
+	Copyright 2002-2011 by Pascal Francq (pascal@francq.info).
 	Copyright 2002-2004 by Julien Lamoral.
 	Copyright 2002-2004 by David Wartel.
 	Copyright 2002-2008 by the Université Libre de Bruxelles (ULB).
