398ce3b95c935e039cc7cf4d6d474a8ca38c183a
pfrancq <pfrancq@5a340755-d22d-0410-935e-977bf2ceaed6>
2011-01-24 16:10:26 +0000

    - Update from 2010 tp 2011.

--- feedback/feedback.cpp
+++ feedback/feedback.cpp
@@ -6,7 +6,7 @@
 
 	Feedback-based Profile Computing Method - Implementation.
 
-	Copyright 2001-2010 by Pascal Francq (pascal@francq.info).
+	Copyright 2001-2011 by Pascal Francq (pascal@francq.info).
 	Copyright 2001-2008 Université Libre de Bruxelles (ULB).
 
 	This library is free software; you can redistribute it and/or
