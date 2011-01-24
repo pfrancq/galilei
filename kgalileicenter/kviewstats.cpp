8165e74cca82303172695ba139fd79501c6f959b
pfrancq <pfrancq@038c31d4-cd2b-0410-b4d5-8dc527888777>
2011-01-24 16:10:20 +0000

    - Update from 2010 tp 2011.

--- kgalileicenter/kviewstats.cpp
+++ kgalileicenter/kviewstats.cpp
@@ -6,7 +6,7 @@
 
 	Window for the running some instruction to the session - Implementation.
 
-	Copyright 2002-2010 by Pascal Francq (pascal@francq.info).
+	Copyright 2002-2011 by Pascal Francq (pascal@francq.info).
 	Copyright 2002-2008 by the Université Libre de Bruxelles (ULB).
 
 	This library is free software; you can redistribute it and/or
--- kgalileicenter/qpluginslist.cpp
+++ kgalileicenter/qpluginslist.cpp
@@ -6,7 +6,7 @@
 
 	Widget to configure a list of plug-ins - Implementation.
 
-	Copyright 2008-2010 by Pascal Francq (pascal@francq.info).
+	Copyright 2008-2011 by Pascal Francq (pascal@francq.info).
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU Library General Public
