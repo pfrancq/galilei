19ec7f4be84f72064d1df55fd65a41e209ae4dae
pfrancq <pfrancq@61adec9e-d22d-0410-8196-ac144d880f7c>
2014-11-23 18:56:00 +0000

    - Adapt to the change of the cmake macro defined by R.

--- sugs/frontend/sugs_frontend.cmake
+++ sugs/frontend/sugs_frontend.cmake
@@ -1,5 +1,5 @@
 IF(KDE4_FOUND)
     SET(sugs_AVAILABLE_FRONTENDS kde)
-    SET(sugs_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(sugs_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(sugs_frontend sugs kde sugs_INCLUDES)
 ENDIF(KDE4_FOUND)
