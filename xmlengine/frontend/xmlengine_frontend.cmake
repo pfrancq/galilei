cd2b8ed99003461d314fdf858bf73acfca5cfa31
pfrancq <pfrancq@6e637571-214b-0410-83ce-d2ee30c7c22b>
2014-11-23 18:57:07 +0000

    - Adapt to the change of the cmake macro defined by R.

--- xmlengine/frontend/xmlengine_frontend.cmake
+++ xmlengine/frontend/xmlengine_frontend.cmake
@@ -1,5 +1,5 @@
 IF(KDE4_FOUND)
     SET(xmlengine_AVAILABLE_FRONTENDS kde)
-    SET(xmlengine_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(xmlengine_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(xmlengine_frontend xmlengine kde xmlengine_INCLUDES)
 ENDIF(KDE4_FOUND)
