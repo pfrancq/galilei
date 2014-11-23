ec9bc21cab5a97dfe71d3e2e44eedd7b7bb44ffe
pfrancq <pfrancq@938b8ebe-d22d-0410-8726-cfdb544c6d2e>
2014-11-23 18:56:15 +0000

    - Adapt to the change of the cmake macro defined by R.

--- stopwords/frontend/stopwords_frontend.cmake
+++ stopwords/frontend/stopwords_frontend.cmake
@@ -1,6 +1,6 @@
 IF(KDE4_FOUND)
     SET(stopwords_AVAILABLE_FRONTENDS kde)
-    SET(stopwords_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(stopwords_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(stopwords_frontend stopwords kde stopwords_INCLUDES)
 ENDIF(KDE4_FOUND)
 
--- stringindexer/frontend/gstringindexer_frontend.cmake
+++ stringindexer/frontend/gstringindexer_frontend.cmake
@@ -1,6 +1,6 @@
 IF(KDE4_FOUND)
     SET(gstringindexer_AVAILABLE_FRONTENDS kde)
-    SET(gstringindexer_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(gstringindexer_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(gstringindexer_frontend gstringindexer kde gstringindexer_INCLUDES)
 ENDIF(KDE4_FOUND)
 
