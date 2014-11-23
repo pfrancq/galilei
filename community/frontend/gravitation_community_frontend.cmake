d6db0e84165dcb0844b1e4207d8cf76d5511b539
pfrancq <pfrancq@217aa3a9-d22d-0410-8724-f61aaa32e77c>
2014-11-23 18:48:01 +0000

    - Adapt to the change of the cmake macro defined by R.

--- community/frontend/gravitation_community_frontend.cmake
+++ community/frontend/gravitation_community_frontend.cmake
@@ -1,5 +1,5 @@
 IF(KDE4_FOUND)
     SET(gravitation_community_AVAILABLE_FRONTENDS kde)
-    SET(gravitation_community_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(gravitation_community_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(gravitation_community_frontend gravitation_community kde gravitation_community_INCLUDES)
 ENDIF(KDE4_FOUND)
--- topic/frontend/gravitation_topic_frontend.cmake
+++ topic/frontend/gravitation_topic_frontend.cmake
@@ -1,5 +1,5 @@
 IF(KDE4_FOUND)
     SET(gravitation_topic_AVAILABLE_FRONTENDS kde)
-    SET(gravitation_topic_kde_DEPENDENCY rcorekde ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
+    SET(gravitation_topic_kde_DEPENDENCY rcoreqt ${KDE4_KDEUI_LIBS} ${KDE4_KIO_LIBS})
     ADD_FRONTEND_SOURCE_FILE(gravitation_topic_frontend gravitation_topic kde gravitation_topic_INCLUDES)
 ENDIF(KDE4_FOUND)
