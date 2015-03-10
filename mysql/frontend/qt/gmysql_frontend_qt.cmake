71183a304ecc8f2cec100c71165eae1a3042c12d
pfrancq <pfrancq@53e8738f-d22d-0410-9854-d053d8e46257>
2015-03-10 18:19:46 +0000

    - Code cleanup.

--- mysql/frontend/qt/gmysql_frontend_qt.cmake
+++ mysql/frontend/qt/gmysql_frontend_qt.cmake
@@ -14,9 +14,4 @@ FOREACH(file ${gmysql_frontend_qt_Qt_INCLUDES})
    QT4_WRAP_CPP(gmysql_frontend_qt_UIFILE ${REALFILE})
 ENDFOREACH(file ${gmysql_frontend_qt_Qt_INCLUDES})
 
-#FOREACH(file ${gmysql_frontend_kde_UI_INCLUDES})
-#   SET(REALFILE frontend/kde/${file})
-#   KDE4_ADD_UI_FILES(gmysql_frontend_kde_UIFILE ${REALFILE})
-#ENDFOREACH(file ${gmysql_frontend_kde_UI_INCLUDES})
-#
 INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR})
