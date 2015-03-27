64ec433b1d1f2e39060fb3a1e6dd0bb34c170037
pfrancq <pfrancq@27a68596-d22d-0410-b208-99edf1ca307c>
2015-03-27 13:52:06 +0000

    - Modify the configuration dialog box.

--- subslevel/frontend/qt/subslevel_qt.cpp
+++ subslevel/frontend/qt/subslevel_qt.cpp
@@ -49,9 +49,8 @@ class Config : public QDialog, public Ui_Config
 public:
 	Config(void)
 	{
+		setupUi(this);
 		setWindowTitle("Configure Level Computation");
-		QWidget* widget=new QWidget(this);
-		setupUi(widget);
 		connect(buttonBox,SIGNAL(accepted()),this,SLOT(accept()));
 		connect(buttonBox,SIGNAL(rejected()),this,SLOT(reject()));
 		adjustSize();
