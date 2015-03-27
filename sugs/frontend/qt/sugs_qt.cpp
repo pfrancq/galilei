963efeb12a47a49df7efe46e56500635a6b58a15
pfrancq <pfrancq@61adec9e-d22d-0410-8196-ac144d880f7c>
2015-03-27 13:54:08 +0000

    - Modify the configuration dialog box.

--- sugs/frontend/qt/sugs_qt.cpp
+++ sugs/frontend/qt/sugs_qt.cpp
@@ -49,9 +49,8 @@ class Config : public QDialog, public Ui_Config
 public:
 	Config(void)
 	{
-		setWindowTitle("Configure Suggestions Plug-In");
-		QWidget* widget=new QWidget(this);
-		setupUi(widget);
+		setupUi(this);
+		setWindowTitle("Configure Suggestions");
 		connect(buttonBox,SIGNAL(accepted()),this,SLOT(accept()));
 		connect(buttonBox,SIGNAL(rejected()),this,SLOT(reject()));
 		adjustSize();
