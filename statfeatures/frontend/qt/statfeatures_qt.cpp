70924ad2bfc73b92f0f9b0e1b4f85989edc64677
pfrancq <pfrancq@3b52b3f3-eae3-4da7-8452-dfcf60b645a9>
2015-03-27 13:51:52 +0000

    - Modify the configuration dialog box.

--- statfeatures/frontend/qt/statfeatures_qt.cpp
+++ statfeatures/frontend/qt/statfeatures_qt.cpp
@@ -36,9 +36,8 @@
 //------------------------------------------------------------------------------
 Config::Config(void)
 {
-	setWindowTitle("Configure E-mail Plug-In");
-	QWidget* widget=new QWidget(this);
-	setupUi(widget);
+	setupUi(this);
+	setWindowTitle("Configure Features Statistics");
 	connect(buttonBox,SIGNAL(accepted()),this,SLOT(accept()));
 	connect(buttonBox,SIGNAL(rejected()),this,SLOT(reject()));
 	connect(Edit,SIGNAL(pressed()),this,SLOT(chooseFile()));
